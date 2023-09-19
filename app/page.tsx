4a2c4c348784b306084ca4c707109de352d50493
Boris Arkenaar <boris@arkenaar.com>
2023-09-19 12:25:30 +0100

    Remove tailwind, add icon, contact links

--- app/page.tsx
+++ app/page.tsx
@@ -1,238 +1,306 @@
 import Image from "next/image";
+import { IconBrandInstagram, IconMail } from "@tabler/icons-react";
+
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <h1 className="mb-16 text-3xl font-light uppercase text-center">
-        Female Gaze Model
-      </h1>
-      <h2 className="mb-4 text-2xl font-medium uppercase text-center">
-        Portfolio
-      </h2>
-      <div className="flex flex-col gap-4">
-        <Image
-          src="https://femalegaze.pages.dev/100.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/200.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/300.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/400.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/500.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/600.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/700.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/800.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/900.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1000.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1100.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1200.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1300.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1400.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1500.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1600.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1700.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1800.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/1900.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2000.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2100.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2200.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2300.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2400.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2500.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2600.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2700.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2800.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/2900.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3000.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3100.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3200.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3300.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3400.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3500.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3600.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-        <Image
-          src="https://femalegaze.pages.dev/3700.jpg"
-          alt=""
-          width="640"
-          height="640"
-        />
-      </div>
-    </main>
+    <>
+      <main className={styles.container}>
+        <div className={styles.inner}>
+          <h1 className={styles.header}>Female&nbsp;Gaze by&nbsp;Yana</h1>
+          <nav className={styles.nav}>
+            <div className={styles.contact}>
+              <a href="https://instagram.com/ecstatic.yogini" title="Instagram">
+                <IconBrandInstagram size={24} stroke={1} />
+              </a>
+              <a href="mailto:shanti.info@protonmail.com" title="Email">
+                <IconMail size={24} stroke={1} />
+              </a>
+            </div>
+            <a href="#about" className={styles.about}>
+              About me
+            </a>
+          </nav>
+          <h2 className={styles.subHeader}>My&nbsp;Model Portfolio</h2>
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://femalegaze.pages.dev/100.jpg"
+              alt=""
+              width="640"
+              height="744"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/200.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/300.jpg"
+              alt=""
+              width="640"
+              height="958"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/400.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/500.jpg"
+              alt=""
+              width="640"
+              height="907"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/600.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/700.jpg"
+              alt=""
+              width="640"
+              height="884"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/800.jpg"
+              alt=""
+              width="640"
+              height="765"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/900.jpg"
+              alt=""
+              width="640"
+              height="759"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1000.jpg"
+              alt=""
+              width="640"
+              height="959"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1100.jpg"
+              alt=""
+              width="640"
+              height="1066"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1200.jpg"
+              alt=""
+              width="640"
+              height="959"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1300.jpg"
+              alt=""
+              width="640"
+              height="640"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1400.jpg"
+              alt=""
+              width="640"
+              height="511"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1500.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1600.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1700.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1800.jpg"
+              alt=""
+              width="640"
+              height="640"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/1900.jpg"
+              alt=""
+              width="640"
+              height="400"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2000.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2100.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2200.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2300.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2400.jpg"
+              alt=""
+              width="640"
+              height="616"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2500.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2600.jpg"
+              alt=""
+              width="640"
+              height="400"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2700.jpg"
+              alt=""
+              width="640"
+              height="400"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2800.jpg"
+              alt=""
+              width="640"
+              height="400"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/2900.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3000.jpg"
+              alt=""
+              width="640"
+              height="961"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3100.jpg"
+              alt=""
+              width="640"
+              height="425"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3200.jpg"
+              alt=""
+              width="640"
+              height="426"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3300.jpg"
+              alt=""
+              width="640"
+              height="960"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3400.jpg"
+              alt=""
+              width="640"
+              height="808"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3500.jpg"
+              alt=""
+              width="640"
+              height="800"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3600.jpg"
+              alt=""
+              width="640"
+              height="800"
+            />
+            <Image
+              src="https://femalegaze.pages.dev/3700.jpg"
+              alt=""
+              width="640"
+              height="800"
+            />
+          </div>
+          <article className={styles.article}>
+            <a id="about" />
+            <h2>About me</h2>
+            <p>
+              Hey there! I'm Yana.
+              <br />
+              Nice to see you on my page!
+            </p>
+            <p>
+              I'm a freelance model with more than 10 years of expirience. I'm
+              originally from Russia, but now living in the South of Portugal.
+              Sometimes I make trips around Europe as well. I'm fluently
+              speaking English and Russian.
+            </p>
+            <p>
+              I'm fully natural, no tattoes, have long hair and green eyes.
+              <br />
+              I have a fit flexible body as I'm regularly going to the gym and
+              doing yoga.
+              <br />
+            </p>
+            <p>
+              I'm open for shootings in genres such as portrait, beauty,
+              fashion, lifestyle, lovestory (with my partner only), conceptual
+              art, lingerie, bodypaint, implied art nude.
+            </p>
+            <p>
+              <strong>I DO NOT</strong> pose for:
+              <br />
+              Pornography, erotic, met art, open legs. I do not shoot concepts
+              that are oversexualized or poses that have vulgar or lewd nature
+              (such as sucking a finger, underskirt etc).
+              <br />
+              I reserve the right to refuse to execute poses that I find
+              inappropriate or do not comply with my personal limits and terms.
+              <br />
+            </p>
+            <p>
+              If you have any questions or you would like to book a shooting,
+              please contact me by mail or Instagram.
+            </p>
+            <p>Looking forward to create some epic art together!🔥🫶</p>
+          </article>
+        </div>
+        <footer className={styles.footer}>
+          <p>© 2018 – 2023 Yana</p>
+          <p>
+            Flower background image by{" "}
+            <a href="https://www.freepik.com/free-vector/hand-drawn-flat-design-simple-flower-outline_25261795.htm#page=3&query=lily%20flower&position=42&from_view=keyword&track=ais">
+              Freepik
+            </a>
+          </p>
+        </footer>
+      </main>
+    </>
   );
 }
